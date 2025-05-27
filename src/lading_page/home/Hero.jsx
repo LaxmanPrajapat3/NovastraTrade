ae646fae8e4ffaaf040c6f1015948a7f71220940
Laxman <theprogrammer452023@gmail.com>
2025-05-27 22:40:33 +0530

    Improved responsiveness in React components (second commit)

--- src/lading_page/home/Hero.jsx
+++ src/lading_page/home/Hero.jsx
@@ -5,8 +5,8 @@ export default function Hero(){
       <>   
         <div className="grid grid-cols-1 grid-rows-4 justify-items-center align-items-center  gap-x-4 gap-y-4 grid-rows-[1fr] ">
 <img src={stock1} alt="Hero Image" className='bg-blue-900 mt-15' style={{width:'50vw'}} />
-    <h1 className='investheading' >Invest in Everything</h1>
-    <p className=''>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+    <h1 className='investheading pl-4 pr-4' >Invest in Everything</h1>
+    <p className='pl-4 pr-4'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
     <button className=''>Signup For Free</button>
       </div>
       </>
--- src/lading_page/home/Pricing.jsx
+++ src/lading_page/home/Pricing.jsx
@@ -1,15 +1,15 @@
 export default function Pricing(){
     return(
-        <div className="grid grid-cols-2  text-center pl-5 pr-5 pb-30 h-60  items-center mt-2">
+        <div className="grid grid-cols-2  text-center pl-5 pr-5 pb-30 h-60  items-center mt-2  max-sm:grid-rows-2 max-sm:grid-cols-1 max-sm:h-100" >
             <div className="">
                 
-                <h1 className="text-2xl bold">Unbeatable pricing</h1>
-                <p className="text-xs mt-4 mb-4">
+                <h1 className="text-2xl bold max-sm:text-lg">Unbeatable pricing</h1>
+                <p className="text-xs mt-4 mb-4  max-sm:mb-0 max-sm:mt-0 ">
                    We pioneered the concept of discount broking and price transparency in India. Flat fees and no hidden charges. 
                 </p>
                 <a href="See pricing text-xs">See pricing &nbsp; <i class="fa-solid fa-arrow-right"></i></a>
                 </div>
-                <div className="">
+                <div className="max-sm:mt-29">
                     <img src="../../src/assets/price_img_1.png" alt="" className="h-60 w-full object-contain"/>
                 </div>
         </div>
