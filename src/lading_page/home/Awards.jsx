e01b75a1ce2430794464c66cd2a5422843b18df0
Laxman <theprogrammer452023@gmail.com>
2025-05-28 22:51:44 +0530

    add some new componets

--- src/lading_page/home/Awards.jsx
+++ src/lading_page/home/Awards.jsx
@@ -1,15 +1,36 @@
 export default function Awards(){
     return(
-        <div className="">
-            <div className="">
+        <div className="grid grid-cols-3 grid-rows-1  text-center p-7 max-sm:grid-cols-1 max-sm:grid-rows-2 max-sm:justify-items-center">
+            <div className=" p-2 mb-2" >
 
+<img src="../../src/assets/Award_img_1.png" alt="" className="h-80 justify-self-center" />
+            </div>
 
+            <div className=" p-2 col-span-2 mt-4" >
+              <b>
+                
+                <h1 className=" text-2xl pt-1 pb-1">Largest Stock broker in india</h1>
+                </b>  
+<p className="text-base pb-1">2+ millon NovastraTrade clients contribute to over 15% of all retail order volumes in India daily by trading and investing in:</p>
 
-            <div className="">Awards</div>
+<div className=" grid grid-cols-2 grid-rows-1 text-center max-sm:grid-cols-1 max-sm:grid-rows-2">
+<ul className="list-disc ">
+    <li >Futures and Options</li>
+    <li>Commodity derivatives</li>
+    <li>Currency derivatives</li>
+    
+    </ul> 
+    <ul className="list-disc">
+        
+    <li>Stocks & IPOs</li>
+    <li>Direct mutualfunds </li>
+    <li>Bonds</li>
+    
+     </ul>               </div>
+                </div>
            
 
 
-            </div>
         </div>
     )
 }
--- src/lading_page/home/Stats.jsx
+++ src/lading_page/home/Stats.jsx
@@ -1,5 +1,39 @@
+import './Stats.css'
 export default function Stats(){
     return(
-        <h2>Stats</h2>
+        <div className="grid grid-cols-2 grid-rows-1 bg-gray-200 p-5 gap-4">
+<div className="bg-sky-300 grid grid-rows-4 gap-4 ">
+    <h1>Trust with confidence</h1>
+    <div  className="paragraps">
+    <h3>Customer-first always</h3>
+    <p>That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments and contribute to 15% of daily retail exchange volumes in India.</p>
+        
+    </div>
+    <div  className="paragraps">
+
+    <h3>No spam or gimmicks</h3>
+    <p>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like.</p>
+
+    </div>
+    <div className="paragraps">
+
+
+    <h3>The Zerodha universe</h3>
+    <p>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
+
+    </div>
+    <div className="paragraps">
+
+    <h3>Do better with money</h3>
+    <p>With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
+        
+    </div>
+
+</div>
+<div className="bg-green-300">
+    <img src="../../src/assets/Stats_img_1.png" alt="" />
+</div>
+
+        </div>
     )
 }
